174380c148a992a08a08b4b81906916296501854
Elio petrini <petrieli@students.zhaw.ch>
2024-11-23 11:49:55 +0100

    Praktikum 9

--- code.js
+++ code.js
@@ -4,9 +4,10 @@ let state = {
     [ '', '', '', '', '', '', '' ],
     [ '', '', '', '', '', '', '' ],
     [ '', '', '', '', '', '', '' ],
-    [ '', 'b', '', '', '', '', '' ],
-    [ '', 'r', '', 'b', '', '', '' ]
-    ]
+    [ '', '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '', '' ]
+    ],
+    activeColor: "r"
    }
 
 
@@ -33,6 +34,7 @@ function showBoard() {
         boardContainer.appendChild(createBoardCell(cellValue));
       });
     });
+    updateCurrentPlayer()
 }
 
 
@@ -43,5 +45,63 @@ function createBoardCell(value) {
       : value === "b"
       ? elt("div", { class: "blue piece" }, "")
       : null;
-  return elt("div", { class: "field" }, ...(piece ? [piece] : []));
+    
+    let returnNode = elt("div", { class: "field" }, ...(piece ? [piece] : []))
+
+    if (piece == null) {
+        function handleCellClick(event) {
+            //need to find which column i'm at
+            const cells = Array.from(document.querySelector(".board").children)
+            const index = cells.indexOf(event.target)
+            const column = 6-index%7
+            let updatedeCell = setPieceOnCell(returnNode, column);
+            updatedeCell.removeEventListener("mousedown", handleCellClick);
+        }
+        returnNode.addEventListener("mousedown", handleCellClick)
+    }    
+    return returnNode;
+}
+
+function setPieceOnCell(returnNode,column) {
+    let currentColor = state.activeColor
+    let piece
+    if (currentColor === "r") {
+        piece = elt("div", { class: "red piece" }, "")
+        state.activeColor = "b"
+    } else if (currentColor === "b") {
+        piece = elt("div", { class: "blue piece" }, "")
+        state.activeColor = "r"
+    }
+    updateCurrentPlayer()  
+    let cellToBeUpdated = findNextFreeCellInColumn(returnNode, column, currentColor)
+    cellToBeUpdated.appendChild(piece)
+    return cellToBeUpdated
+}
+
+function findNextFreeCellInColumn(cell, column, currentColor) {
+    let cellToBeUpdated
+    const cells = Array.from(document.querySelector(".board").children)
+    for (let row = state.board.length-1; row >= 0; row--) {
+       if (state.board[row][column] == "") {
+            state.board[row][column] = currentColor
+            let cellDivIndex = (row)*7+(6-column)
+            cellToBeUpdated = cells[cellDivIndex]
+            break;
+       }
+    }
+    return cellToBeUpdated
+}
+
+function resetBoard() {
+    state.board = Array(6).fill('').map(el => Array(7).fill(''))
+    showBoard()
+}
+
+function updateCurrentPlayer() {
+    if (state.activeColor === "r") {
+        document.querySelector(".whosturn").innerHTML = "Red player's turn"
+    } else {
+        document.querySelector(".whosturn").innerHTML = "Blue player's turn"
+    }
+
 }
